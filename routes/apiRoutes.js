fe5aec3bfa6a5a16c1274de8a55e49304f6c2fb4
Rudy Sanchez <rudys212@gmail.com>
2021-07-18 02:37:11 -0700

    completed post rt

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
   // API GET Requests
 
   app.get("/api/notes", (req, res) => {
-    fsAsync("db/db.json", "utf8").then((notesData) => {
+    fsAsync("./db/db.json", "utf8").then((notesData) => {
       console.log(typeof notesData);
       res.json(JSON.parse(notesData));
     });
@@ -16,16 +16,22 @@ module.exports = (app) => {
 
   // API POST requests
   app.post("/api/notes", (req, res) => {
-    const newNote = req.body;
-    fsAsync("db/db.json", "utf8");
+    let notesEntered = req.body;
+    let notesId = data.length.toString();
+    console.log(notesEntered);
+    fs.writeFileSync("./db/db.json", JSON.stringify(data), (err, data) => {
+      if (err) throw err;
+    });
   });
+  res.send("Notes is now posted");
 
   // DELETE route
 
-  //   app.post("/api/notes/:id", (req, res) => {
-  // Empty out the arrays of data
-  //     data.length = 0;
+  app.delete("/api/notes/:id", (req, res) => {
+    const deleteNotes = deleteId(req.params.id);
 
-  //     res.json({ ok: true });
-  //   });
+    if (deleteNotes === -1) return res.status(404).json({});
+    notes.splice(deleteNotes, 1);
+    res.JSON(notes);
+  });
 };
